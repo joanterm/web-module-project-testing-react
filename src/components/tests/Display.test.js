955a6f32870d97e307bc29792397b1f3ff9e37c2
joanterm <jo_joanarg@yahoo.com>
2022-05-23 19:31:58 -0400

    Finished project

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,10 +1,58 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import userEvent from "@testing-library/user-event"
 import '@testing-library/jest-dom/extend-expect';
 import Display from './../Display';
+import mockFetch from "./../../api/fetchShow"
+jest.mock("./../../api/fetchShow")
 
-test('renders without errors with no props', async () => { });
+{/* <Display displayFun={displayFunc}/> */}
 
-test('renders Show component when the button is clicked ', () => { });
+const dummyShowData = {
+    name: "test name",
+    summary: "test summary",
+    seasons: [
+        {
+            id: "1",
+            name: "season name one",
+            episodes: []
+        },
+        {
+            id: "2",
+            name: "season name two",
+            episodes: []
+        }
+    ]
+}
 
-test('renders show season options matching your data when the button is clicked', () => { });
+test('renders without errors with no props', async () => {
+    render(<Display />)
+ });
+
+test('renders Show component when the button is clicked ', async () => {
+    mockFetch.mockResolvedValueOnce(dummyShowData)
+    render(<Display />)
+    const button = screen.getByRole("button")
+    userEvent.click(button)
+    const show = await screen.findByTestId("show-container")
+    expect(show).toBeInTheDocument()
+
+ });
+
+test('renders show season options matching your data when the button is clicked', async () => {
+    mockFetch.mockResolvedValueOnce(dummyShowData)
+    render(<Display />)
+    const button = screen.getByRole("button")
+    userEvent.click(button)
+    await waitFor(() => {
+        const seasonOptions = screen.queryAllByTestId("season-option")
+        expect(seasonOptions).toHaveLength(2)
+    })
+ });
+
+
+// Test that the Display component renders without any passed in props.
+// Rebuild or copy the show test data element as used in the previous set of tests.
+// Test that when the fetch button is pressed, the show component will display. Make sure to account for the api call and change of state in building your test.
+// Test that when the fetch button is pressed, the amount of select options rendered is equal to the amount of seasons in your test data.
+// Notice the optional functional prop passed in to the Display component client code. Test that when the fetch button is pressed, this function is called.
--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,8 +3,48 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { });
+{/* <Episode episode={episode} /> */}
 
-test("renders the summary test passed as prop", () => { });
+const dummyTest = {
+    //this could be any data pulled out from API
+    id: 1,
+    name: "",
+    image: "https://i.ibb.co/2FsfXqM/stranger-things.png",
+    season: 1,
+    number: 1,
+    summary: "test summary not in dom",
+    runtime: 1
+}
 
-test("renders default image when image is not defined", () => { });
+const dummyTestWithoutImage = {
+    //this could be any data pulled out from API
+    id: 1,
+    name: "",
+    image: null,
+    season: 1,
+    number: 1,
+    summary: "test summary not in dom",
+    runtime: 1
+}
+
+test("renders without error", () => { 
+    render(<Episode episode={dummyTest}/>)
+});
+
+test("renders the summary test passed as prop", () => {
+    render(<Episode episode={dummyTest}/>)
+    const summary = screen.queryByText(/test summary not in dom/i)
+    expect(summary).toBeInTheDocument()
+    expect(summary).toBeTruthy()
+    expect(summary).toHaveTextContent("test summary not in dom")
+ });
+
+test("renders default image when image is not defined", () => {
+    render(<Episode episode={dummyTestWithoutImage} />)
+    const image = screen.queryByAltText('https://i.ibb.co/2FsfXqM/stranger-things.png')
+    expect(image).toBeInTheDocument()
+ })
+
+// Complete a test that shows the Episode component renders. Pass in the provided example episode data as a test prop.
+// Modify the test data to display a specific summary statement. Complete a test that shows that the summary value passed in to the Episode component displays as expected. Use at least then 3 different types of expect statements to test the the existence of the summary value.
+// The episode component displays a default value ('https://i.ibb.co/2FsfXqM/stranger-things.png') when a image url is not provided. Create a new piece of test data with the image property set to null. Test that the alt tag of the image displayed is set to './stranger_things.png'.
